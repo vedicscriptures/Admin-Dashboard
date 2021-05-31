17e9943ec9d2f2f28e013c33488ccd72fa6d6dc4
ptprashanttripathi <ptprashanttripathi@outlook.com>
2021-05-31 10:50:43 +0530

    first commit

---
+++ routes/routes.js
@@ -0,0 +1,14 @@
+// import routes of GITA
+const bhagavadgitaRoutes = require('./bhagavadgitaroutes');
+const Routes = (app, fs) => {
+
+    // default route to vedicscripters.github.io
+    app.get('/', (req, res) => {
+	res.redirect('https://vedicscriptures.github.io/');
+	});
+
+    // gita routes
+    bhagavadgitaRoutes(app,fs);
+};
+
+module.exports = Routes;
---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const mongoose = require('mongoose');
+const fs = require('fs');
+const reqLimit = require('./models/reqLimiter');
+const auth = require('./models/auth');
+require('dotenv').config();
+
+app.all('*', function (req, res, next) {
+    res.set('Access-Control-Allow-Origin', '*');
+    next();
+});
+app.set('trust proxy', 1);
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use('/gita/*',auth,reqLimit);
+mongoose.connect(process.env.MONGOURI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useFindAndModify: false,
+        useCreateIndex: true
+}).then(() => console.log("connected to MONGO DB...")).catch(() => console.log(err));
+
+const routes = require('./routes/routes.js')(app, fs);
+const PORT = process.env.PORT || 80;
+const server = app.listen(PORT, () => {
+    console.log('listening on port %s...', server.address().port);
+});
